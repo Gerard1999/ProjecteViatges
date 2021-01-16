dde05b3d7f9408cca660fb2a5f8bccfaa78f23b9
Gerard López <46751836+Gerard1999@users.noreply.github.com>
2021-01-16 21:13:39 +0100

    AplicaDescompte

--- WonderfulTravel/WebContent/js/destinacions.js
+++ WonderfulTravel/WebContent/js/destinacions.js
@@ -1,22 +1,22 @@
 const continents = [
 
   {continent: "Europa", paisos:[
-    {nom: "Alemanya", preu: "500.00", img: "./images/alemanya.jpg"},
-    {nom: "Itàlia", preu: "900,00", img: "./images/italia.jpg"}]},
+    {nom: "Alemanya", preu: "500", img: "./images/alemanya.jpg"},
+    {nom: "Itàlia", preu: "900", img: "./images/italia.jpg"}]},
 
   {continent: "Amèrica", paisos:[
-    {nom: "Cuba", preu: "1000,00", img: "./images/cuba.jpg"},
-    {nom: "Canada", preu: "2000,00", img: "./images/canada.jpg"}]},
+    {nom: "Cuba", preu: "1000", img: "./images/cuba.jpg"},
+    {nom: "Canada", preu: "2000", img: "./images/canada.jpg"}]},
 
   {continent: "Àsia", paisos:[
-    {nom: "Japó", preu: "3000,00", img: "./images/japo.jpg"},
-    {nom: "Thailandia", preu: "2000,00", img: "./images/thailandia.jpg"}]},
+    {nom: "Japó", preu: "3000", img: "./images/japo.jpg"},
+    {nom: "Thailandia", preu: "2000", img: "./images/thailandia.jpg"}]},
 
   {continent: "Àfrica", paisos: [
-    {nom: "Algèria", preu: "1200,00", img: "./images/algeria.jpeg"},
-    {nom: "Madagascar", preu: "2600,00", img: "./images/madagascar.jpg"}]},
+    {nom: "Algèria", preu: "1200", img: "./images/algeria.jpeg"},
+    {nom: "Madagascar", preu: "2600", img: "./images/madagascar.jpg"}]},
 
   {continent: "Oceania", paisos: [
-    {nom: "Nova Zelanda", preu: "4000,00", img: "./images/new_zealand.jpg"},
-    {nom: "Illes Cook", preu: "3100,00", img: "./images/cook_islands.jpg"}]}
+    {nom: "Nova Zelanda", preu: "4000", img: "./images/new_zealand.jpg"},
+    {nom: "Illes Cook", preu: "3100", img: "./images/cook_islands.jpg"}]}
 ];
--- WonderfulTravel/WebContent/js/validacioFormulari.js
+++ WonderfulTravel/WebContent/js/validacioFormulari.js
@@ -28,16 +28,31 @@ function omplirPais(){
   });
 }
 
-//NO ESTÀ ACABAT!
+//Omplir preu i foto un cop seleccionat el país
 function omplirPreuFoto(){
   document.getElementById("pais").addEventListener('change', (event) => {
     var paisSelect = event.target.value;
-  
+
     for (let i = 0; i < continents.length; i++) {
       for (let x = 0; x < continents[i].paisos.length; x++) {
         var pais = continents[i].paisos[x].nom;
         if (pais == paisSelect) {
-          document.getElementById("preu").value = continents[i].paisos[x].preu + "€";
+          var preu = continents[i].paisos[x].preu;
+          
+          var descompte =  document.getElementById("descompte").checked;
+          if (descompte) {
+            document.getElementById("preu").value = ((parseInt(preu) * 20) / 100).toString() + ",00€";
+          } else {
+            document.getElementById("preu").value = continents[i].paisos[x].preu + ",00€";
+          }
+          document.getElementById("descompte").addEventListener('change', (event) => {
+            var descompte =  document.getElementById("descompte").checked;
+            if (descompte) {
+              document.getElementById("preu").value = ((parseInt(preu) * 20) / 100).toString() + ",00€";
+            } else {
+              document.getElementById("preu").value = continents[i].paisos[x].preu + ",00€";
+            }
+          });
           document.getElementById("foto").innerHTML = "<img height=250 src='" + continents[i].paisos[x].img + "'>";
         }
       } 
