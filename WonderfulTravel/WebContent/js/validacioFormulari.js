cdd97d44d13f4d9458d817493edf8ff2a02e568d
Gerard López <46751836+Gerard1999@users.noreply.github.com>
2021-01-16 17:11:13 +0100

    Canvia el preu i es posa la foto

--- WonderfulTravel/WebContent/js/validacioFormulari.js
+++ WonderfulTravel/WebContent/js/validacioFormulari.js
@@ -12,13 +12,14 @@ function omplirContinents(){
 //Omplir països a la llista un cop seleccionat el continent
 function omplirPais(){
   document.getElementById("continent").addEventListener('change', (event) => {
-    var llistaPais = "";
+    var llistaPais = "<option disabled selected>Països</option>";
     var indexContinent = event.target.selectedIndex;
   
     for (let i = 0; i < continents.length; i++) {
       if (i == indexContinent) {
         for (let x = 0; x < continents[i].paisos.length; x++) {
-          llistaPais += "<option value'" + continents[i].paisos[x].nom + "'>" + continents[i].paisos[x].nom + "</option>";
+          var id = continents[i].paisos[x].id;
+          llistaPais += "<option value'" + continents[i].paisos[x].id + "'>" + continents[i].paisos[x].nom + "</option>";
         }
       } 
     }
@@ -28,20 +29,19 @@ function omplirPais(){
 }
 
 //NO ESTÀ ACABAT!
-function omplirPreu(){
+function omplirPreuFoto(){
   document.getElementById("pais").addEventListener('change', (event) => {
-    var preu = "";
-    var indexPreu = event.target.selectedIndex;
+    var paisSelect = event.target.value;
   
     for (let i = 0; i < continents.length; i++) {
       for (let x = 0; x < continents[i].paisos.length; x++) {
-        //if (x == indexPreu) {
-          preu = document.getElementById("preu").innerHTML = continents[i].paisos[x].preu;
-        //}
+        var pais = continents[i].paisos[x].nom;
+        if (pais == paisSelect) {
+          document.getElementById("preu").value = continents[i].paisos[x].preu + "€";
+          document.getElementById("foto").innerHTML = "<img height=250 src='" + continents[i].paisos[x].img + "'>";
+        }
       } 
     }
-    document.getElementById("preu").innerHTML = preu;
-
   });
 }
 
